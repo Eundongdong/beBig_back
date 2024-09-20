a4e377430722cde3bab22762c83fa0fed5843794
jisuLeeeee <120107671+jisuLeeeee@users.noreply.github.com>
2024-09-20 17:12:07 +0900

    bebig-112 feat
    커뮤니티 인터페이스, 구현체, 매퍼클래스 생성

---
+++ src/main/java/beBig/mapper/CommunityMapper.java
@@ -0,0 +1,7 @@
+package beBig.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public class CommunityMapper {
+}
---
+++ src/main/java/beBig/service/CommunityService.java
@@ -0,0 +1,13 @@
+package beBig.service;
+
+import beBig.vo.PostVo;
+
+import java.util.List;
+
+public interface CommunityService {
+    public List<PostVo> showList();
+    public PostVo showDetail(int postId);
+    public void write(PostVo post);
+    public void update(PostVo post);
+    public void delete(int postId);
+}
