8f6e234bd4fd9eea5f9ed2aad34282190502fa46
Eundongdong <51512016+Eundongdong@users.noreply.github.com>
2024-09-24 10:54:24 +0900

    [BEBIG-64] build : aws s3 config
    S3 config 추가
    Multipart resolver추가
    build.gradle에 multipart 관련 설정 추가

--- src/main/java/beBig/config/S3Config.java
+++ src/main/java/beBig/config/S3Config.java
@@ -9,8 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
-@PropertySource({"classpath:/application-aws.propert" +
-        "ies"})
+@PropertySource({"classpath:/application-aws.properties"})
 @Configuration
 public class S3Config {
     @Value("${cloud.aws.credentials.access-key}")
