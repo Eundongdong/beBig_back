ef0d14d8317d045806924923364f555dd472826c
Eundongdong <51512016+Eundongdong@users.noreply.github.com>
2024-10-08 15:04:12 +0900

    [BEBIG-212] build : ip 변경

--- src/main/java/beBig/config/WebConfig.java
+++ src/main/java/beBig/config/WebConfig.java
@@ -49,7 +49,7 @@ public class WebConfig extends AbstractAnnotationConfigDispatcherServletInitiali
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true); // 인증 정보 허용
-        config.addAllowedOrigin("http://localhost:5173"); // 프론트엔드 도메인 허용
+        config.addAllowedOrigin("http://3.35.68.38:80"); // 프론트엔드 도메인 허용
         config.addAllowedHeader("*"); // 모든 헤더 허용
         config.addAllowedMethod("*"); // 모든 HTTP 메서드 허용
         source.registerCorsConfiguration("/**", config);
